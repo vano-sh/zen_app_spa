feaf193eeb3fdc22bdb424dea298792b0afcf137
Shiryaev Ivan <vano-sh@mail.ru>
2022-12-01 23:04:22 +0300

    main

--- src/components/layout/ProgressBar/ProgressBar.js
+++ src/components/layout/ProgressBar/ProgressBar.js
@@ -1,3 +1,31 @@
+import { useEffect } from 'react'
+
 export const ProgressBar = () => {
-  return <div className='progress-bar'></div>
+  useEffect(() => {
+    const heightDocument = window.outerHeight
+    const heightWindow = window.innerHeight
+    console.log({ window })
+    console.log()
+    console.log(heightDocument)
+    console.log(heightWindow)
+
+    const width = Math.round(heightDocument / heightWindow)
+
+    console.log(width)
+  }, [])
+
+  const width = () => {
+    const heightDocument = window.outerHeight
+    const heightWindow = window.innerHeight
+
+    return Math.round(heightDocument / heightWindow)
+  }
+
+  return (
+    <div
+      className='progress-bar'
+      style={{
+        width: width(),
+      }}></div>
+  )
 }
